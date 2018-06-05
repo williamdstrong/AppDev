bfe9ea32a3b081425650b7e928a6f0e4c8b45452
William Strong <william.strong@liferay.com>
2018-06-05 14:58:45 -0700

    Create mvc-portlet with Blade

---
+++ modules/amf-admin-monitor/amf-admin-monitor-web/src/main/java/com/liferay/training/amf/monitor/portlet/constants/AmfAdminMonitorWebPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.training.amf.monitor.portlet.constants;
+
+/**
+ * @author liferay
+ */
+public class AmfAdminMonitorWebPortletKeys {
+
+	public static final String AmfAdminMonitorWeb = "AmfAdminMonitorWeb";
+
+}
---
+++ modules/amf-admin-monitor/amf-admin-monitor-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="amf-admin-monitor-web.caption"/></b>
+</p>
