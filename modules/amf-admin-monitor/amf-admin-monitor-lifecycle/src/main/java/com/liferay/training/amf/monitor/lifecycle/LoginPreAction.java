8874c3822590dd2b3638fba64f03e6d98fae5f84
William Strong <william.strong@liferay.com>
2018-06-04 17:51:45 -0700

    Add lifecycle package

--- modules/amf-admin-monitor/amf-admin-monitor-lifecycle/src/main/java/com/liferay/training/amf/monitor/lifecycle/LoginPreAction.java
+++ modules/amf-admin-monitor/amf-admin-monitor-lifecycle/src/main/java/com/liferay/training/amf/monitor/lifecycle/LoginPreAction.java
@@ -1,4 +1,35 @@
 package com.liferay.training.amf.monitor.lifecycle;
 
-public class LoginPreAction {
+import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.events.LifecycleEvent;
+import com.liferay.training.amf.monitor.service.AdminMonitorLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.log.LogService;
+
+/**
+ * @author William Strong
+ *
+ */
+@Component(
+        immediate = true,
+        property = "key=login.events.pre",
+        service = LifecycleAction.class
+)
+public class LoginPreAction implements LifecycleAction {
+
+    @Override
+    public void processLifecycleEvent(LifecycleEvent lifecycleEvent)
+        throws ActionException {
+
+
+    }
+
+    @Reference
+    private LogService _log;
+
+    @Reference
+    protected AdminMonitorLocalService adminMonitorLocalService;
+
 }
