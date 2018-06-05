d35d20a1ddadc273ff9790418dd0532a62048e98
William Strong <william.strong@liferay.com>
2018-06-05 16:11:16 -0700

    Create MainRenderCommand

--- modules/amf-admin-monitor/amf-admin-monitor-web/src/main/java/com/liferay/training/amf/monitor/portlet/commands/MainRenderCommand.java
+++ modules/amf-admin-monitor/amf-admin-monitor-web/src/main/java/com/liferay/training/amf/monitor/portlet/commands/MainRenderCommand.java
@@ -1,4 +1,26 @@
 package com.liferay.training.amf.monitor.portlet.commands;
 
-public class MainRenderCommand {
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.training.amf.monitor.portlet.constants.AdminMonitorPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+@Component(
+	immediate = true,
+	property = {
+			"javax.portlet.name=" + AdminMonitorPortletKeys.AmfAdminMonitorWeb,
+			"mvc.command.name=/"
+	},
+	service = MVCRenderCommand.class
+)
+
+public class MainRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(RenderRequest request, RenderResponse response) {
+
+		return "/view.jsp";
+	}
 }
