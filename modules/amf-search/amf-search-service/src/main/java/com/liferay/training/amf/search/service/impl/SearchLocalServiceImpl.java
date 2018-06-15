bfc09bb40003b0118d26ef398727e270605bf434
William Strong <william.strong@liferay.com>
2018-06-15 11:42:11 -0700

    refactor from remote service to local service - no changes

--- modules/amf-search/amf-search-service/src/main/java/com/liferay/training/amf/search/service/impl/SearchLocalServiceImpl.java
+++ modules/amf-search/amf-search-service/src/main/java/com/liferay/training/amf/search/service/impl/SearchLocalServiceImpl.java
@@ -14,7 +14,21 @@
 
 package com.liferay.training.amf.search.service.impl;
 
+import com.liferay.portal.kernel.dao.orm.*;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Address;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.training.amf.search.dto.SearchData;
+import com.liferay.training.amf.search.exception.InvalidZipCodeException;
+import com.liferay.training.amf.search.exception.NoSearchQueryException;
+import com.liferay.training.amf.search.service.SearchService;
 import com.liferay.training.amf.search.service.base.SearchLocalServiceBaseImpl;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * The implementation of the search local service.
@@ -31,9 +45,138 @@ import com.liferay.training.amf.search.service.base.SearchLocalServiceBaseImpl;
  * @see com.liferay.training.amf.search.service.SearchLocalServiceUtil
  */
 public class SearchLocalServiceImpl extends SearchLocalServiceBaseImpl {
+	private static Log _log = LogFactoryUtil.getLog(SearchService.class.getName());
+	private Integer size;
+
 	/*
 	 * NOTE FOR DEVELOPERS:
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.training.amf.search.service.SearchLocalServiceUtil} to access the search local service.
 	 */
+	public List<SearchData> findByZip(String zip, int start, int end)
+			throws PortalException {
+
+		validateZip(zip);
+		return getFormattedData(zip, start, end);
+	}
+
+	public long getSize() throws NoSearchQueryException {
+		if (size == null) {
+			throw new NoSearchQueryException();
+		} else {
+			return size;
+		}
+	}
+
+	private void validateZip(String zip) throws InvalidZipCodeException {
+		if (!Validator.isNotNull(zip)) {
+			throw new InvalidZipCodeException.Null();
+		}
+		if (!Validator.isNumber(zip)) {
+			throw new InvalidZipCodeException.NotANumber();
+		}
+		if (isFewerThanFiveDigits(zip)) {
+			throw new InvalidZipCodeException.TooFewDigits();
+		}
+		if (isGreaterThanFiveDigits(zip)) {
+			throw new InvalidZipCodeException.TooManyDigits();
+		}
+	}
+
+	private boolean isFewerThanFiveDigits(String zip) {
+		return zip.length() < 5;
+	}
+
+	private boolean isGreaterThanFiveDigits(String zip) {
+		return zip.length() > 5;
+	}
+
+	private List<SearchData> getFormattedData(String zip, int start, int end) {
+		if (zip.isEmpty()) {
+			return new LinkedList<>();
+		}
+		List<User> users;
+		List<SearchData> searchData = new LinkedList<>();
+
+		// Get total size and set.
+		size = (int) _getSize(zip);
+
+		try {
+			users = getUsers(zip, start, end);
+		} catch (PortalException e) {
+			return searchData;
+		}
+
+		SearchData a;
+		for (User u : users) {
+			SearchData s = new SearchData(getFirstName(u), getLastInitial(u), getScreenName(u), getEmailAddress(u));
+			searchData.add(s);
+//			searchData.add(new SearchData(getFirstName(u), getLastInitial(u), getScreenName(u), getEmailAddress(u)));
+		}
+		return searchData;
+	}
+
+	private String getFirstName(User u) {
+		return u.getFirstName();
+	}
+
+	private String getLastInitial(User u) {
+		String lastName = u.getLastName();
+		return String.valueOf(lastName.charAt(0));
+	}
+
+	private String getScreenName(User u) {
+		return u.getScreenName();
+	}
+
+	private String getEmailAddress(User u) {
+		return u.getEmailAddress();
+	}
+
+	private long _getSize(String zip) {
+		return findUsersByZipCount(zip);
+	}
+
+	private List<User> getUsers(String zip, int start, int end) throws PortalException {
+		return findUsersByZip(zip, start, end);
+	}
+
+	private long findUsersByZipCount(String zip) {
+		DynamicQuery zipQuery =
+				DynamicQueryFactoryUtil.forClass(Address.class)
+						.add(RestrictionsFactoryUtil.eq("zip", zip));
+		return addressPersistence.countWithDynamicQuery(zipQuery);
+	}
+
+	private List<User> findUsersByZip(String zip, int start, int end) throws PortalException {
+
+		// Use dynamic query that finds all entries with a particular zip code.
+
+		List<Long> userIds = getUserIdsByZip(zip, start, end);
+		List<User> users = new LinkedList<>();
+		for (Long l : userIds) {
+			User u = userLocalService.getUser(l);
+			users.add(u);
+		}
+		return users;
+	}
+
+	private List<Long> getUserIdsByZip(String zip, int start, int end) {
+
+		Session session = null;
+		try {
+			session = addressPersistence.openSession();
+
+			DynamicQuery zipQuery =
+					DynamicQueryFactoryUtil.forClass(Address.class)
+							.add(RestrictionsFactoryUtil.eq("zip", zip))
+							.setProjection(ProjectionFactoryUtil.property("userId"));
+			return addressPersistence.findWithDynamicQuery(zipQuery, start, end);
+		} catch (ORMException e) {
+			_log.error("Cannot get address data.");
+			return new LinkedList<>();
+		} finally {
+			addressPersistence.closeSession(session);
+		}
+	}
 }
--- modules/amf-search/amf-search-service/src/main/java/com/liferay/training/amf/search/service/impl/SearchServiceImpl.java
+++ modules/amf-search/amf-search-service/src/main/java/com/liferay/training/amf/search/service/impl/SearchServiceImpl.java
@@ -14,22 +14,14 @@
 
 package com.liferay.training.amf.search.service.impl;
 
-import com.liferay.portal.kernel.dao.orm.*;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Address;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.training.amf.search.dto.SearchData;
 import com.liferay.training.amf.search.exception.InvalidZipCodeException;
 import com.liferay.training.amf.search.exception.NoSearchQueryException;
-import com.liferay.training.amf.search.service.SearchService;
 import com.liferay.training.amf.search.service.base.SearchServiceBaseImpl;
 import com.liferay.training.amf.search.service.permission.SearchServicePermissionChecker;
 
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -69,134 +61,10 @@ public class SearchServiceImpl extends SearchServiceBaseImpl {
 
 		SearchServicePermissionChecker.check(getPermissionChecker(), groupId, "USE_THIS_SERVICE");
 
-		validateZip(zip);
-		return getFormattedData(zip, start, end);
+		return searchLocalService.findByZip(zip, start, end);
 	}
 
 	public long getSize() throws NoSearchQueryException {
-		if (size == null) {
-			throw new NoSearchQueryException();
-		}
-		else {
-			return size;
-		}
+		return searchLocalService.getSize();
 	}
-
-	private void validateZip(String zip) throws InvalidZipCodeException {
-		if (!Validator.isNotNull(zip)) {
-			throw new InvalidZipCodeException.Null();
-		}
-		if (!Validator.isNumber(zip)) {
-			throw new InvalidZipCodeException.NotANumber();
-		}
-		if (isFewerThanFiveDigits(zip)) {
-			throw new InvalidZipCodeException.TooFewDigits();
-		}
-		if (isGreaterThanFiveDigits(zip)) {
-			throw new InvalidZipCodeException.TooManyDigits();
-		}
-	}
-
-	private boolean isFewerThanFiveDigits(String zip) {
-		return zip.length() < 5;
-	}
-
-	private boolean isGreaterThanFiveDigits(String zip) {
-		return zip.length() > 5;
-	}
-
-	private List<SearchData> getFormattedData(String zip, int start, int end) {
-		if (zip.isEmpty()) {
-			return new LinkedList<>();
-		}
-		List<User> users;
-		List<SearchData> searchData = new LinkedList<>();
-
-		// Get total size and set.
-		size = (int)_getSize(zip);
-
-		try {
-			users = getUsers(zip, start, end);
-		} catch (PortalException e) {
-			return searchData;
-		}
-
-		SearchData a;
-		for (User u : users) {
-			SearchData s = new SearchData(getFirstName(u), getLastInitial(u), getScreenName(u), getEmailAddress(u));
-			searchData.add(s);
-//			searchData.add(new SearchData(getFirstName(u), getLastInitial(u), getScreenName(u), getEmailAddress(u)));
-		}
-		return searchData;
-	}
-
-	private String getFirstName(User u) {
-		return u.getFirstName();
-	}
-
-	private String getLastInitial(User u) {
-		String lastName = u.getLastName();
-		return String.valueOf(lastName.charAt(0));
-	}
-
-	private String getScreenName(User u) {
-		return u.getScreenName();
-	}
-
-	private String getEmailAddress(User u) {
-		return u.getEmailAddress();
-	}
-
-	private long _getSize(String zip) {
-		return findUsersByZipCount(zip);
-	}
-
-	private List<User> getUsers(String zip, int start, int end) throws PortalException {
-		return findUsersByZip(zip, start, end);
-	}
-
-	private long findUsersByZipCount(String zip) {
-		DynamicQuery zipQuery =
-				DynamicQueryFactoryUtil.forClass(Address.class)
-						.add(RestrictionsFactoryUtil.eq("zip", zip));
-		return addressPersistence.countWithDynamicQuery(zipQuery);
-	}
-
-	private List<User> findUsersByZip(String zip, int start, int end) throws PortalException {
-
-		// Use dynamic query that finds all entries with a particular zip code.
-
-		List<Long> userIds = getUserIdsByZip(zip, start, end);
-		List<User> users = new LinkedList<>();
-		for (Long l : userIds) {
-			User u = userLocalService.getUser(l);
-			users.add(u);
-		}
-		return users;
-	}
-
-	private List<Long> getUserIdsByZip(String zip, int start, int end) {
-
-		Session session = null;
-		try {
-			session = addressPersistence.openSession();
-
-			DynamicQuery zipQuery =
-					DynamicQueryFactoryUtil.forClass(Address.class)
-							.add(RestrictionsFactoryUtil.eq("zip", zip))
-							.setProjection(ProjectionFactoryUtil.property("userId"));
-			return addressPersistence.findWithDynamicQuery(zipQuery, start, end);
-		}
-		catch (ORMException e) {
-			_log.error("Cannot get address data.");
-			return new LinkedList<>();
-		}
-		finally {
-			addressPersistence.closeSession(session);
-		}
-	}
-
-	private Integer size;
-
-	private static Log _log = LogFactoryUtil.getLog(SearchService.class.getName());
 }
