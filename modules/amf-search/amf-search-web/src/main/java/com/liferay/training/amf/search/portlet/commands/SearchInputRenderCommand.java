92ea384c6ccc02e2988802eeb34c9bfa4875740b
William Strong <william.strong@liferay.com>
2018-06-18 09:59:48 -0700

    Add permissions to portlet

--- modules/amf-search/amf-search-web/src/main/java/com/liferay/training/amf/search/portlet/commands/SearchInputRenderCommand.java
+++ modules/amf-search/amf-search-web/src/main/java/com/liferay/training/amf/search/portlet/commands/SearchInputRenderCommand.java
@@ -1,10 +1,14 @@
 package com.liferay.training.amf.search.portlet.commands;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.training.amf.search.constants.AmfSearchPortletKeys;
 import org.osgi.service.component.annotations.Component;
 
+import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -18,8 +22,44 @@ import javax.portlet.RenderResponse;
 )
 public class SearchInputRenderCommand implements MVCRenderCommand {
 
+	private static final String _portlet = "com_liferay_training_amf_search_portlet_AmfSearchPortlet";
+	private static final String _canMakeSearch = "CAN_MAKE_SEARCH";
+
+	private static PermissionChecker _getPermissionChecker(ThemeDisplay themeDisplay) {
+		if (null == themeDisplay) {
+			throw new IllegalArgumentException("request is null");
+		}
+		return themeDisplay.getPermissionChecker();
+	}
+
+	private static long _getGroupId(ThemeDisplay themeDisplay) {
+		if (null == themeDisplay) {
+			throw new IllegalArgumentException("request is null");
+		}
+		return themeDisplay.getScopeGroupId();
+	}
+
+	private static ThemeDisplay _getThemeDisplay(PortletRequest request) {
+		if (null == request) {
+			throw new IllegalArgumentException("request is null");
+		}
+
+		return (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+	}
+
 	@Override
 	public String render(RenderRequest request, RenderResponse response) {
+
+		ThemeDisplay themeDisplay = _getThemeDisplay(request);
+		PermissionChecker permissionChecker = _getPermissionChecker(themeDisplay);
+		long groupId = _getGroupId(themeDisplay);
+
+		boolean hasPermission = permissionChecker.hasPermission(
+				groupId, _portlet, groupId, _canMakeSearch);
+		if (!hasPermission) {
+			return "/NoPermission.jsp";
+		}
+
 
 		SessionMessages.clear(request);
 		return "/SearchInput.jsp";
